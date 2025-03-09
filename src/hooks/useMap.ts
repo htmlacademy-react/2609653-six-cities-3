5802a6e0c8a742595fa41ea0f41012deefc35235
volgavr <vol-gavr@yandex.ru>
2025-03-10 01:04:55 +0300

    Update useMap.ts

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -6,29 +6,35 @@ import { MAP_ZOOM_DEFAULT } from '../const';
 function useMap(
   mapRef: MutableRefObject<HTMLElement | null>,
   city: City
-): Map | null {
+) {
   const [map, setMap] = useState<Map | null>(null);
   const isRenderedRef = useRef<boolean>(false);
 
   useEffect(() => {
-    const mapConfig = {
-      center: {
-        lat: city.lat,
-        lng: city.lng
-      },
-      zoom: MAP_ZOOM_DEFAULT
+    const createMapInstance = () => {
+      const mapConfig = {
+        center: {
+          lat: city.lat,
+          lng: city.lng,
+        },
+        zoom: MAP_ZOOM_DEFAULT,
+      };
+      return new Map(mapRef.current!, mapConfig);
     };
 
-    const layer = new TileLayer(
-      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-      {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      }
-    );
+    const addTileLayer = (mapInstance: Map) => {
+      const layer = new TileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        }
+      );
+      mapInstance.addLayer(layer);
+    };
 
     if (mapRef.current !== null && !isRenderedRef.current) {
-      const instance = new Map(mapRef.current, mapConfig);
-      instance.addLayer(layer);
+      const instance = createMapInstance();
+      addTileLayer(instance);
       setMap(instance);
       isRenderedRef.current = true;
     }
