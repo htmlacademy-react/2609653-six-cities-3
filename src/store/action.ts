9010cdd49aedeec15c0fea826258461a0b0bdc22
volgavr <vol-gavr@yandex.ru>
2025-03-21 15:41:06 +0300

    Delete src/store/action.ts

--- src/store/action.ts
+++
@@ -1,7 +0,0 @@
-//import { createAction } from '@reduxjs/toolkit';
-//import { City2 } from '../types/city';
-
-//export const selectCity = createAction('offers/setCity');
-//export const loadOffers = createAction('city/offers');
-
-//export { setCity };
