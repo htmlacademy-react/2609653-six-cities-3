17b12d9c9e8a03ae928dee4927acd6f5edfb14f9
volgavr <vol-gavr@yandex.ru>
2025-06-01 16:54:27 +0300

    fix

--- src/components/Layout/Header/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-type HeaderAvatarProps = {
-  src?: string;
-}
-
-export default function Avatar({ src }: HeaderAvatarProps) {
-  return (<div className="header__avatar-wrapper user__avatar-wrapper" style={{backgroundImage: src && `url(${src})` }}></div>);
-}
