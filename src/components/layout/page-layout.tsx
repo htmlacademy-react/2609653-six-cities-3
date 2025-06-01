fba7daab1997c9d7ac42914f85e1ddc038236833
volgavr <vol-gavr@yandex.ru>
2025-06-01 17:00:09 +0300

    add

---
+++ src/components/layout/page-layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet, useLocation } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import Header from './header/Header';
+
+function PageLayout() {
+  const location = useLocation();
+  return(
+    <div className={`page page--gray ${location.pathname === AppRoute.Login as string ? 'page--login' : 'page--main'}`}>
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
+
+export default PageLayout;
